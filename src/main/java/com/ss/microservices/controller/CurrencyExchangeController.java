1e41ae0bedf59bf2bf04dff6a1f2c97c5a626589
Sadanala <ssadanala@deloitte.com>
2021-03-01 15:04:20 +0530

    port changes using Environment interface

--- src/main/java/com/ss/microservices/controller/CurrencyExchangeController.java
+++ src/main/java/com/ss/microservices/controller/CurrencyExchangeController.java
@@ -2,7 +2,9 @@ package com.ss.microservices.controller;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,13 +16,16 @@ import com.ss.microservices.dto.CurrencyExchange;
 @RequestMapping("currency-exchange")
 public class CurrencyExchangeController {
 	
-	@Value("${server.port}")
-	private String port;
+//	@Value("${server.port}")
+//	private String port;
+	
+	@Autowired
+	Environment environment;
 
 	@GetMapping(value = "/from/{from}/USD/to/{to}/INR")
 	public CurrencyExchange exchange(@PathVariable String from, @PathVariable String to) {
 		CurrencyExchange currencyExchange = new CurrencyExchange(1000L, "INR", "TO", new BigDecimal(50));
-		currencyExchange.setPort(port);
+		currencyExchange.setPort(environment.getProperty("server.port"));
 		return currencyExchange;
 
 	}
