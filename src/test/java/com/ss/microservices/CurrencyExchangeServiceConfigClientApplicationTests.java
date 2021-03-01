9f8045241d91590f594858239ced4e18463a4420
Sadanala <ssadanala@deloitte.com>
2021-03-01 13:04:15 +0530

    currency exchng service basic structure changes

---
+++ src/test/java/com/ss/microservices/CurrencyExchangeServiceConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
