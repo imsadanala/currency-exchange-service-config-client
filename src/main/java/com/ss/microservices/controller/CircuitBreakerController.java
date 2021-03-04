35cf002684721e1b9b2e66cdce8cbbaedfdfd8dc
imsadanala <ssadanala@deloitte.com>
2021-03-04 14:40:39 +0530

    resilience4j @Retry() changes

--- src/main/java/com/ss/microservices/controller/CircuitBreakerController.java
+++ src/main/java/com/ss/microservices/controller/CircuitBreakerController.java
@@ -1,14 +1,25 @@
 package com.ss.microservices.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class CircuitBreakerController {
+	
+	Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
 	@GetMapping(value = "/breaker")
+	@Retry(name = "default")
 	public String simpleAPI() {
-		return "Hello";
+		logger.info("simpleAPI CALLED");
+		ResponseEntity<String> responseEntity = new RestTemplate().getForEntity("", String.class);
+		return responseEntity.getBody();
 	}
 
 }
